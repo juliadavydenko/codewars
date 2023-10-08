ee8a69462be39450b6293305dd5abcb15ca0c277
juliadavydenko <julietta12345@yahoo.com>
2023-10-08 20:20:48 +0200

    try catch

--- js/asyncawait.js
+++ js/asyncawait.js
@@ -6,3 +6,32 @@ const assets = {
 };
 
 const is_shop_open = true;
+
+// const order = () =>{
+
+//     return new Promise( (resolve, reject)=>{
+//         if(true){
+//             resolve
+//         }
+//         else {
+//             reject()
+//         }
+//     } )
+// }
+
+// order()
+// .then()
+// .then()
+// .then()
+// .catch()
+// .finally()
+
+async function order() {
+  try {
+    await abc;
+  } catch (error) {
+    console.log("abc doesn't exist", error);
+  } finally {
+    console.log("runs anyway");
+  }
+}
