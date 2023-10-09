35f17848243d43429f34d90cc92e825f296866f3
juliadavydenko <julietta12345@yahoo.com>
2023-10-09 19:17:14 +0200

    test

--- js/await.js
+++ js/await.js
@@ -8,5 +8,17 @@ const assets = {
 const is_shop_open = true;
 
 const toppings_choice = () => {
-  return new Promise((resolve, reject));
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("please choose a topping");
+    }, 3000);
+  });
 };
+
+async function kitchen() {
+  console.log("A");
+  console.log("B");
+  console.log("C");
+  console.log("D");
+  console.log("E");
+}
