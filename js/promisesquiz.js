859cdfa00118fc1480d5dc3509b10948e65e3279
juliadavydenko <julietta12345@yahoo.com>
2023-10-16 21:42:03 +0200

    isNan changed

--- js/promisesquiz.js
+++ js/promisesquiz.js
@@ -1,13 +1,17 @@
-// Function receives a parameter (data). Function must return a promise. 
+// Function receives a parameter (data). Function must return a promise.
 // If data is not a number - return promise rejected instantly and give the data error in a string.
 // If data is an odd number, return a promise resolved in 1 second and return "odd" in a string.
-// If data is even, return "even" after 2 seconds 
-
+// If data is even, return "even" after 2 seconds
 
 function job(data) {
-    return new Promise(function(resolve, reject){
-        if (typeof data !== 'number' || is NaN(data)) {
-            reject("error");
-        }
-    })
+  return new Promise(function (resolve, reject) {
+    if (typeof data !== "number" || isNaN(data)) {
+      reject("error");
+    } else if (data % 2 === 0) {
+      setTimeout(() => reject("even"), 2000);
+    } else {
+      setTimeout(() => resolve("odd"), 1000);
+    }
+  });
 }
+job(5);
