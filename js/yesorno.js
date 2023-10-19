c2386f5fbcf0e0e5ba8798b33de28cd4b4c368d6
juliadavydenko <julietta12345@yahoo.com>
2023-10-19 13:51:57 +0200

    promises and async await

--- js/yesorno.js
+++ js/yesorno.js
@@ -1,5 +1,12 @@
+//fetching yes or no from the API using promises
+
 fetch("https://yesno.wtf/api")
-  .then((response) => response.json())
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
   .then((data) => {
     console.log(data.image, data.answer);
   })
@@ -7,4 +14,13 @@ fetch("https://yesno.wtf/api")
     console.error("An error occurred:", error);
   });
 
-//fetching yes or no from the API using async-await
+//fetching yes or no from the API using async await
+
+async function yesOrNo() {
+try {
+    const response = await fetch('https://yesno.wtf/api');
+    if (!response.ok) {
+        throw new Error("There was a problem with the network response")
+    }
+}
+}
