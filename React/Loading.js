c728b828044eb1fa34977808c319060d5345b37a
juliadavydenko <julietta12345@yahoo.com>
2023-10-22 22:08:34 +0200

    added isloading

---
+++ React/Loading.js
@@ -0,0 +1,23 @@
+// useEffect for connecting to external source
+
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
+
+  return <div className="home">{blogs && <BlogList blogs={blogs} />}</div>;
+};
+
+export default Home;
