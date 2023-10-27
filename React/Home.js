5264ba4ab3dfadc64841f0b6a0e73df4b4a2ac87
juliadavydenko <julietta12345@yahoo.com>
2023-10-27 14:22:48 +0200

    modified Home comp according to useFetch changes

---
+++ React/Home.js
@@ -0,0 +1,20 @@
+// useEffect for connecting to external source
+
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+  const { data, isLLoading, error } = useFetch("http://localhost:8000/blogs");
+
+  return (
+    <div className="home">
+      {/* output if have a value for error, conditional rendering (when we set an error) */}
+      {error && <div>{error}</div>}
+      {isLoading && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+    </div>
+  );
+};
+
+export default Home;
