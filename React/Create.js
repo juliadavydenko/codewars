35e1e8ad0af22a9e66473300af3b34b78ccf8e13
juliadavydenko <julietta12345@yahoo.com>
2023-10-29 23:49:30 +0100

    simple form

--- React/Create.js
+++ React/Create.js
@@ -2,7 +2,17 @@ const Create = () => {
   return (
     <div className="create">
       <h2>Add a new thing</h2>
-      <form></form>
+      <form>
+        <label>Title:</label>
+        <input type="text" required />
+        <label>Body:</label>
+        <textarea>required</textarea>
+        <label>Author:</label>
+        <select>
+          <option value="jess">jess</option>
+          <option value="keith">keith</option>
+        </select>
+      </form>
     </div>
   );
 };
